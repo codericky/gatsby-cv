3cf41738dd8c637e7bfa5083065a7f01b384b115
unknown <thevenom425@gmail.com>
2020-12-01 10:41:43 +0700

    update 1-12-2020

--- src/components/header.js
+++ src/components/header.js
@@ -6,31 +6,30 @@ import {Navbar, Nav, Container} from "react-bootstrap"
 const Header = ({ siteTitle }) => (
   <header>
 
-    <Navbar fixed="top" bg="dark" expand="md" variant="dark" >
-    
-    <Container><Navbar.Brand href="/">{siteTitle}</Navbar.Brand>
+    <Navbar fixed="top" bg="info" expand="lg" variant="dark" >
+    <Container>
+      <Navbar.Brand classNa href="/">{siteTitle}</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarResponsive" />
       <Navbar.Collapse id="navbarResponsive" >
      
-      <Container><Nav as="ul" className="ml-auto" >
-        <Nav.Item as="li">
+      <Container>
+        <Nav as="ul" className="ml-auto" >
+        <Nav.Item as="li" className="ml-auto" >
           <Link to="/" className="nav-link" activeClassName="active">Home</Link>
           </Nav.Item>
-          <Nav.Item as="li">
-          <Link to="/work" className="nav-link" activeClassName="active">Work History</Link>
+          <Nav.Item as="li" className="ml-auto">
+          <Link to="/work" className="nav-link" activeClassName="active">Work history</Link>
           </Nav.Item>
-          <Nav.Item as="li">
+          <Nav.Item as="li" className="ml-auto">
           <Link to="/about" className="nav-link" activeClassName="active">About</Link>
           </Nav.Item>
-          <Nav.Item as="li">
+          <Nav.Item as="li" className="ml-auto">
           <Link to="/contact" className="nav-link" activeClassName="active">Contact</Link>
           </Nav.Item>
         </Nav>
         </Container>
-       
     </Navbar.Collapse>
     </Container>
-  
   </Navbar>
   
   
